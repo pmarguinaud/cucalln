160c9258242ac81b44db32279bb50d46295b752a
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2022-11-03 14:01:27 +0000

    First commit

---
+++ src/yomcumfs.F90
@@ -0,0 +1,53 @@
+MODULE YOMCUMFS
+
+IMPLICIT NONE
+
+SAVE
+
+!       ----------------------------------------------------------------
+!**    ** *YOMCUMFS* CONTAINS VARIABLES FOR SIMPLIFIED CONVECTION SCHEME 
+!       ----------------------------------------------------------------
+
+TYPE :: TCUMFS
+LOGICAL :: LECUMFS
+LOGICAL :: LREGCV
+LOGICAL :: LMFCFL2_SHSTEP
+!----------------------------------------------------------------------------
+CONTAINS
+  PROCEDURE, PASS :: PRINT => PRINT_CONFIGURATION 
+END TYPE TCUMFS
+!============================================================================
+
+!!TYPE(TCUMFS), POINTER :: YRCUMFS => NULL()
+
+!*    *YOMCUMFS* CONTAINS VARIABLES FOR SIMPLIFIED CONVECTION SCHEME 
+
+!     P. LOPEZ      E.C.M.W.F.    09/01/2002
+
+!     NAME            TYPE     DESCRIPTION
+!     ----            ----     -----------
+
+!***  LECUMFS         LOGICAL  SWITCH ON SIMPLIFIED CONVECTIVE MASS-FLUX SCHEME IN TRAJECTORY
+!***  LREGCV          LOGICAL  SWITCH ON REGULARIZATIONS OF SIMPLIFIED CONVECTION SCHEME 
+!***                           IN TANGENT-LINEAR AND ADJOINT CALCULATIONS
+!***  LMFCFL2_SHSTEP  LOGICAL  SWITCH ON REDUCTION OF MASS FLUX CFL CRITERION FOR SHORT TIME STEPS 
+
+!     ------------------------------------------------------------------
+CONTAINS
+
+SUBROUTINE PRINT_CONFIGURATION(SELF, KDEPTH, KOUTNO)
+  IMPLICIT NONE
+  CLASS(TCUMFS), INTENT(IN) :: SELF
+  INTEGER      , INTENT(IN) :: KDEPTH
+  INTEGER      , INTENT(IN) :: KOUTNO
+
+  INTEGER :: IDEPTHLOC
+
+  IDEPTHLOC = KDEPTH+2
+
+  WRITE(KOUTNO,*) REPEAT(' ',IDEPTHLOC) // 'LECUMFS = ', SELF%LECUMFS
+  WRITE(KOUTNO,*) REPEAT(' ',IDEPTHLOC) // 'LREGCV = ', SELF%LREGCV
+  WRITE(KOUTNO,*) REPEAT(' ',IDEPTHLOC) // 'LMFCFL2_SHSTEP = ', SELF%LMFCFL2_SHSTEP
+  
+END SUBROUTINE PRINT_CONFIGURATION
+END MODULE YOMCUMFS
