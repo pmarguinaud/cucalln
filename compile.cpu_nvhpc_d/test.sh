23887f9d20b56ae2431e250647955f0e997f8e35
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2023-01-12 17:28:15 +0000

    Prune

--- compile.cpu_nvhpc_d/test.sh
+++ compile.cpu_nvhpc_d/test.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-\rm main_cucalln_mf.x
+set -x
 
-make main_cucalln_mf.x
+nvfortran -mp -byteswapio -Mlarge_arrays -g -O2 -o main_cucalln_mf.nvfortran.x main_cucalln_mf.F90
 
-./main_cucalln_mf.x 
+./main_cucalln_mf.nvfortran.x
 
+ifort \
+  -I. -shared-intel -convert big_endian -assume byterecl \
+  -align array64byte,all -traceback -fpic -qopenmp -qopenmp-threadprivate compat -fp-model source -qopt-report=5 \
+  -qopt-report-phase=vec -ftz -diag-disable=remark,cpu-dispatch -g -O2 -march=core-avx2 -finline-functions \
+  -finline-limit=500 -Winline -qopt-prefetch=4 -fast-transcendentals -fimf-use-svml -no-fma \
+  -o main_cucalln_mf.ifort.x main_cucalln_mf.F90
+
+./main_cucalln_mf.ifort.x
+
+gfortran -fconvert=big-endian -O3 -o main_cucalln_mf.gfortran.x main_cucalln_mf.F90
+
+./main_cucalln_mf.gfortran.x
+   
